98ef56c2ba2077c6eb86085efa6334915cc662ca
EC2 Default User <ec2-user@ip-172-31-76-224.ec2.internal>
2020-09-09 17:18:01 +0000

    prepare configuration test

--- cli/bioims/test/test-configuration.py
+++ cli/bioims/test/test-configuration.py
@@ -7,40 +7,29 @@ import bioims
 configurationClient = bioims.client('configuration')
 
 configurationLambdaArn = configurationClient.getLambdaArn()
-
 print(configurationLambdaArn)
-
+rs = configurationClient.setParameter("key1", "value1")
+rs = configurationClient.setParameter("key2", "value2")
 rs = configurationClient.setParameter("key3", "value3")
-
 print(rs)
-
 r1 = configurationClient.getParameter("key1")
-
 r2 = configurationClient.getParameter("key2")
-
 r3 = configurationClient.getParameter("key3")
-
 print(r1)
-
 print(r2)
-
 print(r3)
-
 ra = configurationClient.getAll()
-
 print(ra)
-
 rh = configurationClient.getHistory("key3")
-
 print(rh)
-
 rst = configurationClient.setDefaultTrainId(0)
-
 print(rst)
-
 rgt = configurationClient.getDefaultTrainId()
-
 print(rgt)
+
+
+
+
 
 
 
