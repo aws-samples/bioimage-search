95c554f81fbcc8dd8afc2b5ed187414a6bda9ee8
EC2 Default User <ec2-user@ip-172-31-76-224.ec2.internal>
2020-10-05 22:40:19 +0000

    working list for bbbc channel files per plate

---
+++ datasets/bbbc-021/scripts/list_plate_image_keys_for_channel.py
@@ -0,0 +1,62 @@
+import sys
+import boto3
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--bucket', type=str, default='', help='bucket with BBBC-021 data')
+parser.add_argument('--channel', type=str, default='', choices=['dapi', 'tubulin', 'actin'], help='must be dapi, tubulin, or actin')
+parser.add_argument('--plate', type=str, default='', help='plate label, e.g., Week_2241')
+
+args = parser.parse_args()
+
+if len(args.bucket) < 1 or\
+    len(args.channel) < 1 or\
+    len(args.plate) < 1:
+    print(args.usage)    
+    exit(1)
+
+imageMetadataKey='BBBC021_v1_image.csv'
+
+s3c = boto3.client('s3')
+
+def getCsvDfFromS3(bucket, key):
+    csvObject = s3c.get_object(Bucket=bucket, Key=key)
+    file_stream = csvObject['Body']
+    df = pd.read_csv(file_stream)
+    return df
+
+image_df = getCsvDfFromS3(args.bucket, imageMetadataKey)
+
+fileDict={}
+
+labelKeyDict={}
+labelKeyDict['dapi']='Image_FileName_DAPI'
+labelKeyDict['tubulin']='Image_FileName_Tubulin'
+labelKeyDict['actin']='Image_FileName_Actin'
+
+labelKey=labelKeyDict[args.channel]
+
+for r1 in range(len(image_df.index)):
+    row1=image_df.iloc[r1]
+    fileDict[row1[labelKey]]=1
+    
+response = s3c.list_objects_v2(
+    Bucket=args.bucket,
+    Prefix=args.plate,
+    MaxKeys=1000
+)
+
+keys=[]
+
+for obj in response['Contents']:
+    k1=obj['Key']
+    k2=k1.split('/')[1]
+    if k2 in fileDict:
+        keys.append(k1)
+        
+for key in keys:
+    print(args.bucket,' ', key)
+    
+        
